9bb31648fdec8fd255d27441240d0b52124a0354
lingmu415 <linggmuo316@gmail.com>
2022-09-14 23:26:01 +0900

    complete ver

---
+++ sophomore/ICO/blockchain/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0xEfb3e4a11D36BC54A2c67074Bacd63e7C050eCD5";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
---
+++ sophomore/nft-collection/blockchain/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xC0Bc093AEEC75298AE2C6a8bb1d4A86b70E235E0";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
